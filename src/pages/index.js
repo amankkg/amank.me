9da92f21626aec4d3327e8478465bcac41bffdea
Aman Kubanychbek <amankubanychbek@gmail.com>
2018-09-04 23:19:20 +0300

    update deps, refactor index links

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,15 @@ import Helmet from 'react-helmet'
 
 import { Layout, Link } from '../components'
 
-const links = [
-  ['/blog/', 'Blogposts'],
-  ['/contacts/', 'Contacts'],
-  ['/cv/', 'CV'],
-]
-
 const IndexPage = () => (
   <Layout>
     <Helmet />
-    <h1>Hi there!</h1>
-    <p>My name is Aman Kubanychbek. I'm a software engineer.</p>
-    {links.reduce((acc, [path, text]) => {
-      if (acc.length > 0) acc.push(' ')
-
-      acc.push(<Link key={path} to={path} text={text} />)
-
-      return acc
-    }, [])}
+    <p>👋 Hey!</p>
+    <Link to="/blog/" text="Blogposts" />
+    &nbsp;
+    <Link to="/contacts/" text="Contacts" />
+    &nbsp;
+    <Link to="/cv/" text="CV" />
   </Layout>
 )
 
