65f8bb3142b333003aa44cd69319d28221b539c1
amankkg <amankubanychbek@gmail.com>
2019-02-17 19:19:45 +0300

    add smartypants plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
           // },
           'gatsby-remark-prismjs',
           // 'gatsby-remark-copy-linked-files',
-          // 'gatsby-remark-smartypants',
+          'gatsby-remark-smartypants',
         ],
       },
     },
